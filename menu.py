f0be2747ca2ef40e26984741760a8a6d17f35c0d
johndao1005 <john.d1729@gmail.com>
2021-09-06 23:20:31 +1200

    working on data control

--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ from tkinter import *
 from PIL import ImageTk,Image
 from tkinter.messagebox import showinfo
 from tkinter import ttk
-import application, setup
+import application, setup, chart_create
 
 
 class window(Tk):
@@ -85,53 +85,49 @@ class window(Tk):
         option1 = setup.pad20
         option2 = setup.pad10
         label = Label(windowname, text=f"White shark {datatype} data").grid(column=0,row=0,**option1,columnspan=3)
-        windowname.geometry(f"800x600+{positionX}+{positionY}")
+        windowname.geometry(f"940x740+{positionX}+{positionY}")
         self.DES_check.add(datatype)
         # ANCHOR data display
         
-        frame = ttk.Frame(windowname)
-        frame.grid(column=0,row=1,**option1,columnspan=2)
-        img = ImageTk.PhotoImage(Image.open(f"./src/{datatype}.png"))
-        panel = Label(frame, text="Hello",width = 50).grid(column=0,row=1,**option1)
-        
+        chart_create.draw_graph(windowname,datatype)
         # Storing new data
         input = StringVar()
         chatLog = StringVar()
         # chat box creating and function
         frame1 = ttk.LabelFrame(windowname,text="Chat box",borderwidth=0)
-        frame1.grid(column=2,row=1,**option1,)
+        frame1.grid(column=2,row=1,**option1,sticky = E)
         chatBox = ttk.Label(frame1,
                         background='light gray',
                         textvariable=chatLog,width = 40
-                        ).grid(column=1,row=2,**option1,columnspan=2)
-        entry = ttk.Entry(frame1, textvariable=input).grid(column=1,row=3,**option1)
+                        ).grid(column=1,row=1,**option1,columnspan=2)
+        entry = ttk.Entry(frame1, textvariable=input).grid(column=1,row=2,**option1,sticky = E)
         button = ttk.Button(frame1,
                         text="Send",
                         command=lambda: chat()
-                        ).grid(column=2,row=3,**option1)
+                        ).grid(column=2,row=2,**option1,sticky = E)
         # ANCHOR Buttons for windowname
         frame2 = ttk.LabelFrame(windowname,text="Chat box",borderwidth=0)
-        frame2.grid(column=2,row=4,**option1,sticky=NSEW)
+        frame2.grid(column=2,row=3,**option1,sticky=NSEW)
         button = ttk.Button(frame2,
                         text="Next",
                         command=lambda: changeWindow(windowname,next)
-                        ).grid(column=0,row=5,**option1)
+                        ).grid(column=0,row=4,**option1)
         button = ttk.Button(frame2,
                         text="Previous",
                         command=lambda: changeWindow(windowname,prev)
+                        ).grid(column=1,row=4,**option1)
+        button = ttk.Button(frame2,
+                        text="Upload",
+                        command=lambda: chat()
                         ).grid(column=1,row=5,**option1)
         button = ttk.Button(frame2,
-                        text="Zoom +",
-                        command=lambda: zoom()
-                        ).grid(column=1,row=6,**option1)
-        button = ttk.Button(frame2,
-                        text="Zoom -",
-                        command=lambda: zoom()
-                        ).grid(column=0,row=7,**option1)
+                        text="Update",
+                        command=lambda: chat()
+                        ).grid(column=0,row=5,**option1)
         Location_windowname = ttk.Button(frame2,
                               text="Pan",
-                              command=lambda: pan()
-                              ).grid(column=1,row=7,**option1)
+                              command=lambda: chat()
+                              ).grid(column=1,row=6,**option1)
         # button = Button(windowname,
         #                 text="upload Data",
         #                 command=lambda: chat()
@@ -139,20 +135,12 @@ class window(Tk):
         button = ttk.Button(windowname,
                         text="Quit",
                         command=lambda: close_DES()
-                        ).grid(column=2,row=8,**option1,sticky=SE)
+                        ).grid(column=2,row=6,**option1,sticky=SE)
         def chat():
             """Place holder for function to update the chat log when there are new chat message
             """
             pass
         
-        def zoom():
-            """Place holder for function to zoom into the data
-            """
-            pass
-        def pan():
-            """Place holder for function to zoom into the data
-            """
-            pass
 
         def changeWindow(current,new):
             current.destroy()
@@ -166,11 +154,11 @@ class window(Tk):
     #ANCHOR creating 3 DES
     def DES1_window(self):
         if "location" not in self.DES_check:
-            self.DES_window(self.DES1,'location',self.DES2_window,self.DES3_window,500,100)
+            self.DES_window(self.DES1,'location',self.DES2_window,self.DES3_window,500,20)
             
     def DES2_window(self):
         if "gender" not in self.DES_check:
-            self.DES_window(self.DES2,'gender',self.DES3_window,self.DES1_window,500,200)
+            self.DES_window(self.DES2,'gender',self.DES3_window,self.DES1_window,500,10)
             
     def DES3_window(self):
         if "feature" not in self.DES_check:
--- test.py
+++ test.py
@@ -1,39 +1,211 @@
+# matplotlib
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+from matplotlib import pyplot as plt
+#tkinter
+import tkinter as tk
+from tkinter import ttk
+#work with url and json file
+from datetime import date
+import requests
+import json
+#pandas and numpy
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
 
-freqs = np.arange(2, 20, 3)
+large_font = ("Verdana",12)
+style.use("ggplot")# other option dark_background,
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.2)
-t = np.arange(0.0, 1.0, 0.001)
-s = np.sin(2*np.pi*freqs[0]*t)
-l, = plt.plot(t, s, lw=2)
+f = Figure()
+a = f.add_subplot(111) # 111 is plot number 1 and 121 is plot number 2
+        
+
+def animate(i):
+    # first version
+    # pullData = open("sample_data.txt","r").read()
+    # dataList = pullData.split("\n")
+    # xList = []
+    # yList = []
+    # for eachLine in dataList:
+    #     if len(eachLine)>1:
+    #         x,y = eachLine.split(",")
+    #         xList.append(int(x))
+    #         yList.append(int(y))
+    # a.clear()
+    # a.plot(xList,yList)
+    
+    
+    # API link
+    # dataLink = 'https://btc-e.com/api/3/trades/btc_usd?limit=2000'#add parameter after the api link with ? and the paramter like limit = 2000
+    # using urllib to open datalink
+    # data = request.urlopen(dataLink)
+    # read data from byte and change to utf-8
+    # print(data)
+    # data = data.readall().decode("utf-8")
+    # print(data)
+    # load the data with json
+    # data = json.loads(data)
+    # # using the key to get the value
+    # data = data["btc_usd"]
+    #  using pandas to change to pandas data set
+    # data = pd.DataFrame(data)
+    
+    # buys = data[(data['type']== "bid")]
+    # buys["datestamp"] = np.array(buys["timestamp"]).astype("datetime64[s]")
+    # buyDates = (buys["datestamp"]).tolist()
+    
+    # sells = data[(data['type']== "ask")]
+    # sells["datestamp"] = np.array(sells["timestamp"]).astype("datetime64[s]")
+    # sellDates = (sells["datestamp"]).tolist()
+    
+    # a.clear()
+    # a.plot_date(buyDates,buys["price"])
+    # a.plot_date(sellDates,sells["price"])
+    
+    # btc ban 
+    dataLink = "https://cex.io/api/trade_history/BTC/USD/?status=200"
+    data = requests.request("GET", dataLink)
+    data = data.json()
+    df = pd.DataFrame(data)
+
+    buys = df[(df['type'] == "buy")]
+    buy_arr = buys['date'].to_numpy()
+    blist = []
+    for i in buy_arr:
+        # print(i)
+        abuy = date.fromtimestamp(int(i))
+        blist.append(abuy)
+
+    buys["datestamp"] = blist
+    print(buys["datestamp"])
+
+    buyDates = (buys["datestamp"]).tolist()
 
 
-class Index:
-    ind = 0
+    sells = df[(df['type'] == "sell")]
+    sell_arr = sells['date'].to_numpy()
+    slist = []
+    for i in sell_arr:
+        # print(i)
+        asell = date.fromtimestamp(int(i))
+        slist.append(asell)
 
-    def next(self, event):
-        self.ind += 1
-        i = self.ind % len(freqs)
-        ydata = np.sin(2*np.pi*freqs[i]*t)
-        l.set_ydata(ydata)
-        plt.draw()
+    sells["datestamp"] = slist
+    sellDates = (sells["datestamp"]).tolist()
 
-    def prev(self, event):
-        self.ind -= 1
-        i = self.ind % len(freqs)
-        ydata = np.sin(2*np.pi*freqs[i]*t)
-        l.set_ydata(ydata)
-        plt.draw()
+    # print(buyDates)
+    # print(sellDates)
 
-callback = Index()
-axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
-axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-bnext = Button(axnext, 'Next')
-bnext.on_clicked(callback.next)
-bprev = Button(axprev, 'Previous')
-bprev.on_clicked(callback.prev)
+    a.clear()
+    a.plot_date(buyDates, buys["price"], "#00a3e0", label="buys")
+    a.plot_date(sellDates, sells["price"], "#183a54", label="sells")
 
-plt.show()
+    a.legend(bbox_to_anchor=(0, 1.02, 1, .102), loc=3, ncol=2, borderaxespad=0)
+
+    title = "BTC-e BTCUSD prices\n Last Price: "+str(df["price"][999])
+    a.set_title(title)
+    
+    
+class App(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        container = tk.Frame(self)
+        container.pack(side = 'top',fill = 'both', expand = True)
+        container.grid_rowconfigure(0,weight = 1)
+        container.grid_columnconfigure(0,weight = 1)
+        
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff = 0)
+        filemenu.add_command(label = "Save settings",command = lambda: popupmsg("Not support yet"))
+        
+        
+        self.frames ={}
+        for Frame in (StartPage,PageOne,PageTwo,PageThree):
+            frame = Frame(container, self)
+            self.frames[Frame] = frame
+            frame.grid(row=0,column = 0, sticky = "nsew")
+        self.show_frame(StartPage)
+        
+    def show_frame(self,cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = ttk.Label(self,text="STart Page")
+        label.pack(pady=10,padx=10)
+        button1 = ttk.Button(self, text ="Visit Page 1",
+                            command=lambda:controller.show_frame(PageOne))
+        button1.pack()
+        button2 = ttk.Button(self, text ="Visit Page 2",
+                            command=lambda:controller.show_frame(PageTwo))
+        button2.pack()
+        button3 = ttk.Button(self, text ="Visit Page 3",
+                            command=lambda:controller.show_frame(PageThree))
+        button3.pack()
+        button4 = ttk.Button(self, text ="Visit Page 4",
+                            command=lambda:controller.show_frame(PageOne))
+        button4.pack()
+        
+class PageOne(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self,text="Page One")
+        label.pack(pady=10,padx=10)
+        button1 = ttk.Button(self, text ="Back To Home",
+                            command=lambda:controller.show_frame(StartPage))
+        button1.pack()
+        button2 = ttk.Button(self, text ="Visit Page 1",
+                            command=lambda:controller.show_frame(PageTwo))
+        button2.pack()
+        
+class PageTwo(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = ttk.Label(self,text="Page Two")
+        label.pack(pady=10,padx=10)
+        button1 = ttk.Button(self, text ="Back To Home",
+                            command=lambda:controller.show_frame(StartPage))
+        button1.pack()
+        button2 = ttk.Button(self, text ="Page PageOne",
+                            command=lambda:controller.show_frame(PageOne))
+        button2.pack()
+        
+class PageThree(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self,text="Graph Page")
+        label.pack(pady=10,padx=10)
+        
+        # graph
+        
+        canvas = FigureCanvasTkAgg(f, self,)
+        canvas.draw()
+        
+        toolbar = NavigationToolbar2Tk(canvas,self)
+        toolbar.update()
+        canvas._tkcanvas.pack()
+        canvas.get_tk_widget().pack(expand=True)
+        
+        # buttons
+        button1 = ttk.Button(self, text ="Back To Home",
+                            command=lambda:controller.show_frame(StartPage))
+        button1.pack()
+        # button2 = ttk.Button(self, text ="Visit Page 1",
+        #                     command=lambda:controller.show_frame(PageOne))
+        # button2.pack()
+        # button3 = ttk.Button(self, text ="Visit Page 1",
+        #                     command=lambda:controller.show_frame(PageTwo))
+        # button3.pack()
+        
+        
+if __name__ == "__main__":    
+    app = App()
+    app.geometry("1280x720")
+    ani = animation.FuncAnimation(f,animate, interval = 1000)
+    app.mainloop()
